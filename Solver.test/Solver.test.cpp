9a8459959e117a94f17d7e9b7188dd9e272acea1
Роман Костин <romankostin22019@gmail.com>
2024-05-24 14:00:36 +0300

    Добавьте файлы проекта.

---
+++ Solver.test/Solver.test.cpp
@@ -0,0 +1,16 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Solvertest
+{
+	TEST_CLASS(Solvertest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
